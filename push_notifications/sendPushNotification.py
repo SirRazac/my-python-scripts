30f7c390b650f6124289e45225c4a7750cab3094
SirRazac <Fabian.Kipp@web.de>
2024-12-28 19:33:32 +0100

    switched frontend into my react app

--- push_notifications/sendPushNotification.py
+++ push_notifications/sendPushNotification.py
@@ -6,7 +6,7 @@ cred = credentials.Certificate("firebaseKey.json")
 firebase_admin.initialize_app(cred)
 
 # Dein Firebase-Gerätetoken (z.B. von einer Web-App, einem Testgerät, etc.)
-device_token = "dROYdjQt3kajZEre1se45F:APA91bHOP10wHOuSIYDwXw8kdXZXE0QSKduCQi6gTXaLHcskWyZn0Y9o27zp-IX2bdYUWc0Aeea1J8ertfU33z5WoxZNqeN4u6gbtKjuOn8ciI5iVAG_PXQ"
+device_token = "dFhaY8RzRHxwc4suWsmUEO:APA91bH6rGX81Gm5IyLkvJNanK8ab8lwe3AfwtrgIXFDB5VJkqW34RnPSSoZhowZN-iZxHzCZSPw6xjlAc9St5MY8abiLXptrGzntIcvBoxuBlHbovQA-h4"
 
 # Erstelle eine Nachricht
 message = messaging.Message(
