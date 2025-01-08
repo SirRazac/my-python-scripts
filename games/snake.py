d66d2231b58f665b411b72fd0a11996742b3d340
SirRazac <Fabian.Kipp@web.de>
2025-01-08 22:15:19 +0100

    Update snake.py

--- games/snake.py
+++ games/snake.py
@@ -1,16 +1,20 @@
-# ------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------ 
 # Import Packages
-# ------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------ 
 import pygame
 
 pygame.init()
 
-# define screen size
+# Define screen size
 WIDTH, HEIGHT = 800, 600
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 clock = pygame.time.Clock()
 
-# game loop
+# Starting position of snake
+snake = [(100, 100), (80, 100), (60, 100)]
+BLOCK_SIZE = 20
+
+# Game loop
 running = True
 while running:
     for event in pygame.event.get():
@@ -18,7 +22,12 @@ while running:
             running = False
 
     # Screen with backcolor
-    screen.fill((255, 255, 255)) # white
+    screen.fill((255, 255, 255))  # white
+
+    # Building snake
+    for segment in snake:
+        pygame.draw.rect(screen, (0, 255, 0), (*segment, BLOCK_SIZE, BLOCK_SIZE))  # green
+
     pygame.display.flip()
     clock.tick(30)
 
